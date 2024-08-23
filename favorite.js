5dc971fe9ec08e7f57fba915870d63ee0b51dca4
Aziz <azizatamuradov1994@gmail.com>
2024-08-23 12:24:39 -0400

    favorite has been added

---
+++ favorite.js
@@ -0,0 +1,14 @@
+
+
+export const favoritePlayer = (player) => {
+
+    return `My favorite player is ${player}`
+}
+
+
+export const favoriteBook = (book) => {
+
+    return `My favorite book is ${book}`
+}
+
+
--- main.js
+++ main.js
@@ -9,6 +9,10 @@ import   sayMyName  from  './name.js'
 
 import myAge from './age.js';
 
+
+
+import *  as favorite  from './favorite.js' 
+
 console.log(sum(3, 6));
 
 console.log(sub(10, 6));
@@ -18,6 +22,8 @@ console.log(sayMyName());
 console.log(myAge());
 
  
+console.log(favorite.favoritePlayer('Ronaldo'));
+console.log(favorite.favoriteBook('Rich Dad, Poor Dad' ));
 
 
 
